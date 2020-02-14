61b31920abb547dbd223bed404a5a7ffbf96b046
wendhio <wendhio.santos@dcomp.ufs.br>
2020-02-14 01:16:29 -0300

    CRUD destinátarios

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,52 @@
+import * as yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      street: yup.string().required(),
+      number: yup.number().required(),
+      complement: yup.string(),
+      state: yup.string().required(),
+      city: yup.string().required(),
+      zipcode: yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validação falhou!' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(401).json('Destinatário já existe!');
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    } = await Recipient.create(req.body);
+
+    return res.status(201).json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    });
+  }
+}
+
+export default new RecipientController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -10,6 +11,8 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
